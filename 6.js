09f35d890fe9e757481a2ed6c3d140bdc64d4569
Ayomide Afolabi <aafolabi@smartterm.io>
2025-04-22 16:52:13 +0100

    assignment

---
+++ 6.js
@@ -0,0 +1,6 @@
+function sorting(items) {
+  return items.sort();
+}
+
+let fruits = ["banana", "apple", "cherry"];
+console.log(sorting(fruits)); 
---
+++ 7.js
@@ -0,0 +1,7 @@
+let numbers = [100, 25, 3, 40];
+
+numbers.sort((a, b) => b - a);
+
+console.log(numbers);
+
+
